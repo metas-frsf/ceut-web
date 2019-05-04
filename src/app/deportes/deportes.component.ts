c215bc7b93d961e69cb3ed39068d2032745d1c78
Esteban Gómez <eggomez@frsf.utn.edu.ar>
2019-05-04 13:38:58 -0300

    Agregar información deportes JEUT

--- src/app/deportes/deportes.component.ts
+++ src/app/deportes/deportes.component.ts
@@ -12,11 +12,11 @@ export class DeportesComponent implements OnInit {
     {
       icono: 'futbol.png',
       nombre: 'Fútbol 5',
-      horario: 'Miércoles 15/5 - 13:00 a 16:00',
+      horario: 'Miércoles 15/5 - 13:00 a 16:00 / Viernes 17/5 - 12:00 a 13:30',
       equipos: 'El torneo constará de 8 equipos de 7 jugadores (5 + 2 suplentes).',
       premios: '',
       reglas: 'Se jugarán 8 partidos de 2 tiempos de 6 minutos cada uno.',
-      descripcion: []
+      descripcion: ['El día viernes se jugarán las semifinales y la final con los equipos que hayan accedido a estas fases.']
     },
     {
       icono: 'basket.png',
@@ -31,9 +31,9 @@ export class DeportesComponent implements OnInit {
       icono: 'truco.png',
       nombre: 'Truco',
       horario: 'Viernes 17/5 - Desde las 10:00',
-      equipos: 'Los equipos estarán compuestos por dos personas',
+      equipos: 'Los equipos estarán compuestos por 2 personas.',
       premios: '',
-      reglas: 'Habrá 4 grupos de 4 equipos cada uno que competirán en formato todos contra todos, pasando a fase eliminatoria los dos mejores de cada grupo',
+      reglas: 'Habrá 4 grupos de 4 equipos cada uno que competirán en formato todos contra todos, pasando a cuartos de final los 2 mejores de cada grupo.',
       descripcion: ['']
     },
     {
@@ -57,37 +57,37 @@ export class DeportesComponent implements OnInit {
     {
       icono: 'volleyball.png',
       nombre: 'Volley',
-      horario: 'Miércoles 15/5 - 16:00 a 17:30',
-      equipos: 'Los equipos estarán compuestos por 6 personas',
+      horario: 'Miércoles 15/5 - 16:00 a 17:30 / Viernes 17/5 - 13:30 a 16:00',
+      equipos: 'Los equipos estarán compuestos por 6 personas.',
       premios: '',
-      reglas: 'Habrá 2 grupos de 3 equipos cada uno en el que competirán todos contra todos, pasando a la final el mejor equipo de cada grupo, jugandose el tercer puesto entre los equipos que quedaran en segunda posición en cada grupo. Los partidos serán de dos tiempos de 5 minutos cada uno',
+      reglas: 'Habrá 2 grupos de 3 equipos cada uno en el que competirán todos contra todos, pasando a la final el mejor equipo de cada grupo, jugándose el tercer puesto entre los equipos que queden en segunda posición en cada grupo. El día viernes se jugarán la final y el tercer puesto. Los partidos serán de dos tiempos de 5 minutos cada uno',
       descripcion: ['']
     },
     {
       icono: 'handball.png',
       nombre: 'Handball',
-      horario: 'Viernes',
-      equipos: 'Los equipos estarán compuestos por 7 personas',
+      horario: 'Viernes 17/5 - 9:30 a 12:00',
+      equipos: 'Los equipos estarán compuestos por 7 personas.',
       premios: '',
-      reglas: 'Habrá 2 grupos de 3 equipos cada uno en el que competirán todos contra todos, pasando a la final el mejor equipo de cada grupo, jugandose el tercer puesto entre los equipos que quedaran en segunda posición en cada grupo. Los partidos serán de dos tiempos de 5 minutos cada uno',
+      reglas: 'Habrá 2 grupos de 3 equipos cada uno en el que competirán todos contra todos, pasando a la final el mejor equipo de cada grupo, jugándose el tercer puesto entre los equipos que quedaran en segunda posición en cada grupo. Los partidos serán de dos tiempos de 5 minutos cada uno',
       descripcion: ['']
     },
     {
       icono: 'ping-pong.png',
       nombre: 'Ping Pong',
-      horario: 'Viernes',
-      equipos: 'Los equipos estarán compuestos por dos personas',
+      horario: 'Viernes 17/5 - 11:00 a 16:00',
+      equipos: 'Los equipos estarán compuestos por 2 personas.',
       premios: '',
-      reglas: 'Habrá 4 grupos de 4 equipos cada uno que competirán en formato todos contra todos, pasando a fase eliminatoria los dos mejores de cada grupo',
+      reglas: 'Habrá 4 grupos de 4 equipos cada uno que competirán en formato todos contra todos, pasando a cuartos de final los 2 mejores de cada grupo',
       descripcion: ['']
     },
     {
       icono: 'ajedrez.png',
       nombre: 'Ajedrez',
-      horario: 'Viernes',
-      equipos: '',
+      horario: 'Viernes 17/5 - 9:30 a 12:00',
+      equipos: 'Cada estudiante competirá individualmente.',
       premios: '',
-      reglas: '',
+      reglas: 'El formato del torneo será de eliminación directa.',
       descripcion: ['']
     },
   ];
