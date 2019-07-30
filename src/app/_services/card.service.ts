81bb4b8cd0d3b8d602c6f652d687e42dc0e42b6a
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2019-07-29 21:24:08 -0300

    Cambiadas tarjetas fijas y actualizada estructura para no ver tarjeta con marquesina

--- src/app/_services/card.service.ts
+++ src/app/_services/card.service.ts
@@ -10,7 +10,7 @@ const headers = new HttpHeaders({
 
 @Injectable()
 export class CardService {
-  private fixedCardListIds = [3, 2]; // Cartas fijadas en la parte superior
+  private fixedCardListIds = [3, 2, 32]; // Cartas fijadas en la parte superior
 
   constructor(public http: HttpClient) {}
 
