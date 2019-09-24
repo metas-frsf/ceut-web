b52c039cb05da8217235e25180864f22bdd2fccd
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2019-09-23 21:01:47 -0300

    Cambios en tarjetas fijadas

--- src/app/_services/card.service.ts
+++ src/app/_services/card.service.ts
@@ -10,7 +10,7 @@ const headers = new HttpHeaders({
 
 @Injectable()
 export class CardService {
-  private fixedCardListIds = [35, 3, 36]; // Cartas fijadas en la parte superior
+  private fixedCardListIds = [2, 3, 18]; // Cartas fijadas en la parte superior
 
   constructor(public http: HttpClient) {}
 
