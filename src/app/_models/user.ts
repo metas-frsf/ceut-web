d09ff2ad0b4f01388f7d362c24320741bda59dff
Ramiro Olivencia <ramiro.olivencia@soteicavisualmesa.com>
2022-07-18 01:49:35 -0300

    Added roles to User model. Added UserRole model and EUserRole enum

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -7,4 +7,18 @@
   token: string;
   avatar: string;
   email: string;
+  roles: UserRole[];
+}
+
+export interface UserRole {
+  id: number;
+  description: string;
+}
+
+export enum EUserRole {
+  ADMINISTRATOR = 1,
+  CEUT_SECRETARY = 2,
+  CEUT_SCHOLAR = 3,
+  CEUT_PRESIDENT = 4,
+  COUNSELOR = 5,
 }
