c5d0ca9be46dfc1dd6c501207207fea5e0094748
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-06-10 00:02:38 -0300

    [#164] - Actualiza Angular a la versión 14

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,12 @@
 ﻿import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+// ToDo: Migrar formularios para que estén tipados
+
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
 import { first } from "rxjs/operators";
 
 import { AlertService, AuthenticationService } from "@app/_services";
@@ -8,16 +14,16 @@ import { HttpClient } from "@angular/common/http";
 
 @Component({
   templateUrl: "login.component.html",
-  styleUrls: ["login.component.scss"]
+  styleUrls: ["login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
+  loginForm: UntypedFormGroup;
   loading = false;
   submitted = false;
   returnUrl: string;
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: UntypedFormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService,
@@ -33,7 +39,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       userName: ["", Validators.required],
-      password: ["", Validators.required]
+      password: ["", Validators.required],
     });
 
     // get return url from route parameters or default to '/'
@@ -59,10 +65,10 @@ export class LoginComponent implements OnInit {
       .login(this.f.userName.value, this.f.password.value)
       .pipe(first())
       .subscribe(
-        data => {
+        (data) => {
           this.router.navigate([this.returnUrl]);
         },
-        error => {
+        (error) => {
           this.alertService.error(error);
           this.loading = false;
         }
