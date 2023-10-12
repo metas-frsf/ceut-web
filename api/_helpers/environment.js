a5010f3ec75b56b877e6f102f9ac117e2c1efc08
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-10-12 15:26:21 -0300

    [#175] - Corrige environment

--- api/_helpers/environment.js
+++ api/_helpers/environment.js
@@ -11,6 +11,7 @@ if (!("error" in result)) {
   environment.sanity.projectId = result.parsed.SANITY_PROJECT_ID;
   environment.sanity.dataset = result.parsed.SANITY_DATASET;
 
+  environment.google = {};
   environment.google.apiKey = result.parsed.GOOGLE_API_KEY;
   environment.google.clientId = result.parsed.GOOGLE_CLIENT_ID;
 } else {
@@ -21,6 +22,7 @@ if (!("error" in result)) {
   environment.sanity.projectId = process.env.SANITY_PROJECT_ID;
   environment.sanity.dataset = process.env.SANITY_DATASET;
 
+  environment.google = {};
   environment.google.apiKey = process.env.GOOGLE_API_KEY;
   environment.google.clientId = process.env.GOOGLE_CLIENT_ID;
 }
