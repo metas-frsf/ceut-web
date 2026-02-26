09df09a709f51e75d2abd92c5ffdbbb32a29a3a5
Ramiro Olivencia <ramiro@olivencia.com.ar>
2026-02-26 08:45:49 -0300

    [HF] - Elimina componente TarjetaAvisosComponent en desuso

--- src/app/tarjetas/tarjetas.module.ts
+++ src/app/tarjetas/tarjetas.module.ts
@@ -1,25 +1,21 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {TarjetasComponent} from './tarjetas.component';
-import {FormsModule} from '@angular/forms';
-import {TarjetaAvisosModule} from './tarjeta-avisos/tarjeta-avisos.module';
-import {TarjetaGenericaModule} from './tarjeta-generica/tarjeta-generica.module';
-import {CardService} from '@app/_services/card.service';
-import {TarjetasRoutingModule} from '@app/tarjetas/tarjetas-routing.module';
-import {LoadingSpinnerModule} from '@app/_components/loading-spinner/loading-spinner.module';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { TarjetasComponent } from "./tarjetas.component";
+import { FormsModule } from "@angular/forms";
+import { TarjetaGenericaModule } from "./tarjeta-generica/tarjeta-generica.module";
+import { CardService } from "@app/_services/card.service";
+import { TarjetasRoutingModule } from "@app/tarjetas/tarjetas-routing.module";
+import { LoadingSpinnerModule } from "@app/_components/loading-spinner/loading-spinner.module";
 
 @NgModule({
   declarations: [TarjetasComponent],
   imports: [
     CommonModule,
     FormsModule,
-    TarjetaAvisosModule,
     TarjetaGenericaModule,
     TarjetasRoutingModule,
     LoadingSpinnerModule,
   ],
-  providers: [
-    CardService
-  ],
+  providers: [CardService],
 })
-export class TarjetasModule { }
+export class TarjetasModule {}
